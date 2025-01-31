bcc2c53c11b5354deab1fab75f1cb337356a6470
Max Genson <accounts@maxgenson.de>
2025-01-31 22:53:01 +0100

    add synth kick

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,11 @@ use embedded_audio_tools as tools;
 mod freeverb;
 mod multi_filter;
 mod simple_delay;
+pub(crate) mod synth_kick;
 
 pub use crate::freeverb::Freeverb;
 pub use crate::multi_filter::MultiFilter;
 pub use crate::simple_delay::SimpleDelay;
+pub use crate::synth_kick::SynthKick;
 
 pub use tools::float::DSPUtility;
